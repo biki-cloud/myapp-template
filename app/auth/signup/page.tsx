4e50b613b7b210955b7ba3765e22ded5607aa23b
hibiki <hibiki123123@gmail.com>
2025-04-30 20:57:28 +0900

    SignInおよびSignUpページにダイナミックレンダリングを強制する設定を追加

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,5 +1,7 @@
 import { SignUpForm } from "@/components/auth/SignUpForm";
 
+export const dynamic = "force-dynamic";
+
 export default function SignUpPage() {
   return (
     <div className="flex min-h-screen items-center justify-center">
